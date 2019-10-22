05ebdc51a5ad4c2b8644f5f4688412927b4317c5
Hector Valdez <hvaldez180@gmail.com>
2019-10-22 18:22:02 -0400

    debugged extra home logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,11 +23,6 @@ display:flex
 export default function Home() {
   return (
     <div>
-      <img
-        className="navbar-logo"
-        alt="Hair Care logo"
-        src="https://img.pngio.com/hair-salon-clipart-hair-stylist-png-hair-extension-logo-ideas-736-hair-stylist-png-images-736_797.jpg"
-      />
 
       <h3>Login</h3>
 
