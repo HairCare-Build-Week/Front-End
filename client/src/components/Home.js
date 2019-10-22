59778dbb67638bbe58301e7df30304a2e5b56469
Hector Valdez <hvaldez180@gmail.com>
2019-10-22 12:58:11 -0400

    debugged home page signup button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,9 +14,11 @@ export default function Home() {
     <div>
       <img className="navbar-logo" alt="Hair Care logo" src="https://img.pngio.com/hair-salon-clipart-hair-stylist-png-hair-extension-logo-ideas-736-hair-stylist-png-images-736_797.jpg"/>
 
+        <NavLink to="/sign-up" className="sign-up-btn">
         <SignupButton> 
           Sign Up
         </SignupButton>
+        </NavLink>
     </div>
   );
 }
