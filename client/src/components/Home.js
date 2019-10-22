20b01ca23768311ae2bab5c04f1c3861783191e6
Hector Valdez <hvaldez180@gmail.com>
2019-10-22 12:56:53 -0400

    my code from home page component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,22 +14,9 @@ export default function Home() {
     <div>
       <img className="navbar-logo" alt="Hair Care logo" src="https://img.pngio.com/hair-salon-clipart-hair-stylist-png-hair-extension-logo-ideas-736-hair-stylist-png-images-736_797.jpg"/>
 
-      <div className="navbar-list">
-        <Link to="/search" className="chosen">
-          Search
-        </Link>
-        <Link to="/my-profile" className="chosen">
-          My Profile
-        </Link>
-        <Link to="/login" className="chosen">
-         Logout/Login
-        </Link> 
-        <NavLink to="/sign-up" className="sign-up-btn">
         <SignupButton> 
           Sign Up
         </SignupButton>
-        </NavLink>
-      </div>
     </div>
   );
 }
