c690c10749dd25e4a05491ec904a22be663765f7
Hector Valdez <hvaldez180@gmail.com>
2019-10-22 13:04:33 -0400

    added city input

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 export default function SignUp(props) {
-
-
   return (
     <div>
       <h1>Sign Up</h1>
 
       <Form>
-
-      <Form.Group controlId="formGroupEmail">
+        <Form.Group controlId="formGroupEmail">
           <Form.Label>Username</Form.Label>
           <Form.Control type="text" placeholder="Username" />
         </Form.Group>
@@ -25,61 +22,9 @@ export default function SignUp(props) {
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
 
-        <Form.Group /*as={Col}*/ controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Control as="select">
-            <option>Choose state</option>
-            <option>Alabama</option>
-            <option>Alaska</option>
-            <option>Arizona</option>
-            <option>Arkansas</option>
-            <option>California</option>
-            <option>Colorado</option>
-            <option>Connecticut</option>
-            <option>Delaware</option>
-            <option>Florida</option>
-            <option>Georgia</option>
-            <option>Hawaii</option>
-            <option>Idaho</option>
-            <option>Illinois</option>
-            <option>Indiana</option>
-            <option>Iowa</option>
-            <option>Kansas</option>
-            <option>Kentucky</option>
-            <option>Louisiana</option>
-            <option>Maine</option>
-            <option>Maryland</option>
-            <option>Massachusetts</option>
-            <option>Michigan</option>
-            <option>Minnesota</option>
-            <option>Mississippi</option>
-            <option>Missouri</option>
-            <option>Montana</option>
-            <option>Nebraska</option>
-            <option>Nevada</option>
-            <option>New Hampshire</option>
-            <option>New Jersey</option>
-            <option>New Mexico</option>
-            <option>New York</option>
-            <option>North Carolina</option>
-            <option>North Dakota</option>
-            <option>Ohio</option>
-            <option>Oklahoma</option>
-            <option>Oregon</option>
-            <option>Pennsylvania</option>
-            <option>Rhode Island</option>
-            <option>South Carolina</option>
-            <option>South Dakota</option>
-            <option>Tennessee</option>
-            <option>Texas</option>
-            <option>Utah</option>
-            <option>Vermont</option>
-            <option>Virginia</option>
-            <option>Washington</option>
-            <option>West Virginia</option>
-            <option>Wisconsin</option>
-            <option>Wyoming</option>
-          </Form.Control>
+        <Form.Group /*as={Col}*/ controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
         </Form.Group>
       </Form>
 
