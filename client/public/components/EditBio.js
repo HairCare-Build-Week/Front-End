ebdb68bf525d178a46cc4717245f8b9f8f2f379c
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-10-24 07:59:02 -0700

    copied files into public so maybe they work

---
+++ client/public/components/EditBio.js
@@ -0,0 +1,96 @@
+import React, {useState, useEffect} from 'react';
+import {axiosWithAuth} from './utilis/axiosWithAuth';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const initialBio = {
+    name: '',
+    salon: '',
+    imageUrl: '',
+    bio: '',
+    address: ''
+}
+
+
+const EditBio = props => {
+    const [editing, setEditing] = useState(false);
+    const [bio, setBio] = useState(initialBio);
+    
+    const handleChange = ev => {
+    ev.persist();
+    let value = ev.target.value;
+    setBio({
+        ...bio,
+        [ev.target.name]: value
+        })
+    }
+
+    useEffect(()=> {
+    const bioToEdit = props.bio;
+    if (bioToEdit) setBio(bioToEdit);
+    }, [props.bio, props.match.params.id]);
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        axiosWithAuth()
+        .put(`${props.stylist}`, bio)
+        .then(res=> {
+            props.updateBio(res.data);
+            props.history.push('/stylist-dash');
+
+        })
+        setEditing(false);
+    };
+
+// const editingBio = bioToEdit  => {
+//     setEditing(true);
+//     setBio(bioToEdit)
+// }
+
+return (
+    <div>
+        <h3>Edit Bio</h3>
+   
+        <EditForm onSubmit={handleSubmit}>
+            <input 
+            name='bio'
+            type='text'
+            onChange={handleChange}
+            value={props.bio}
+            />
+            <div>
+                <p className='edit-btn-aft' onClick={handleSubmit}><Link to='/stylist-dash'>Save</Link></p>
+            </div>
+        </EditForm>
+    
+    </div>
+    )
+}
+
+
+
+export default EditBio;
+
+const EditForm = styled.form`
+    input{
+        height: 250px;
+        width: 400px;
+        margin: 20px;
+    }
+    div{
+        display: flex;
+        justify-content: center
+    }
+    .edit-btn-aft{
+        color: #000;
+        font-size: 1.25rem;
+        border: none;
+        background: none;
+        margin: 0 20px;
+    a{
+        text-decoration: none;
+        color: black;
+        :hover{transform: scale(1.025); color: #80808095; cursor: pointer}
+    }
+`;
---
+++ client/public/components/SearchPage.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import SearchCard from './SearchCard';
+import styled from 'styled-components';
+
+function SearchPage(data) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    // useEffect(()=> {
+    //     const results = data.filter(item=> item.toLowerCase().includes(searchTerm.toLowerCase()));
+    //     setSearchResults(results);
+    // }, [searchTerm])
+
+    const handleChange = e => {
+        setSearchTerm(e.target.value)
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    return(
+        <div>
+            <SearchBar>
+                <h1>Now Viewing Stylists in {data.city}.</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    id='city'
+                    type='text'
+                    name='textfield'
+                    placeholder='Enter City'
+                    value={searchTerm}
+                    onChange={handleChange}/>
+                </form>
+            </SearchBar>
+
+            <SearchContainer>
+                <div className='search-results'>
+                        {searchResults.map(stylist=> (
+                            <SearchCard key = {stylist.id}/>
+                        ))}
+                </div>
+            </SearchContainer>
+        </div>
+    )
+}
+
+export default SearchPage;
+
+
+const SearchContainer = styled.div`
+    margin: 0 auto;  
+    width: 80%;
+    display: flex;
+    border: 1px solid #80808095;
+`;
+
+const SearchBar = styled.form`
+    width: 80%;
+    margin: 10px auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    form{
+        border-bottom: 1.5px solid gray;
+        height: 30px;
+
+        button{
+            background: none;
+            border: 1px solid black;
+            padding: 7px;
+        }
+        input{
+            border: none;
+            width: 200px;
+        }
+    }
+`;
